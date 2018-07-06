14f4109838781ff70eee35dbe972ec4e59fe1144
Pipaolo <mastastah8975@gmailcom>
2018-07-06 12:11:17 +0800

    Use custom audio policy

--- lineage.mk
+++ lineage.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, $(LOCAL_PATH)/device_XT1663.mk)
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device display
 TARGET_SCREEN_HEIGHT := 1920
@@ -15,6 +15,3 @@ PRODUCT_MANUFACTURER := Motorola
 PRODUCT_MODEL := XT1663
 PRODUCT_NAME := lineage_XT1663
 PRODUCT_RELEASE_NAME := XT1663
-
-# Vendor properties
-PRODUCT_RESTRICT_VENDOR_FILES := false
--- product/audio.mk
+++ product/audio.mk
@@ -1,13 +1,14 @@
 # Audio
 PRODUCT_PACKAGES += \
-    audio.r_submix.default \
     audio.a2dp.default \
     audio.usb.default \
-    libaudio-resampler \
+    audio.r_submix.default \
+    libaudiopolicymanagerdefault \
     libtinyalsa \
     libtinycompress \
     libtinymix \
-    libtinyxml
+    libtinyxml \
+    libfs_mgr
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:/system/etc/audio_policy_volumes.xml \
@@ -18,3 +19,11 @@ PRODUCT_COPY_FILES += \
 # Audio
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,device/motorola/XT1663/configs/audio,system/vendor/etc)
+    
+PRODUCT_COPY_FILES += \
+    device/motorola/XT1663/configs/misc_audio/a2dp_audio_policy_configuration.xml:system/etc/a2dp_audio_policy_configuration.xml \
+    device/motorola/XT1663/configs/misc_audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
+    device/motorola/XT1663/configs/misc_audio/audio_policy_volumes.xml:system/etc/audio_policy_volumes.xml \
+    device/motorola/XT1663/configs/misc_audio/default_volume_tables.xml:system/etc/default_volume_tables.xml \
+    device/motorola/XT1663/configs/misc_audio/r_submix_audio_policy_configuration.xml:system/etc/r_submix_audio_policy_configuration.xml \
+    device/motorola/XT1663/configs/misc_audio/usb_audio_policy_configuration.xml:system/etc/usb_audio_policy_configuration.xml 
