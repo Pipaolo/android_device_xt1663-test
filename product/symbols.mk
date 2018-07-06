058b2de55b38efd98da6ee37faf08d45060a4550
Pipaolo <mastastah8975@gmailcom>
2018-07-06 09:19:25 +0800

    Added shim symbols

--- product/symbols.mk
+++ product/symbols.mk
@@ -3,9 +3,15 @@ PRODUCT_PACKAGES += \
     libshim_ui \
     libshim_gui \
     libshim_ifc \
-    libshim_misc
+    libshim_misc \
+    libshim_xlog \
+    libshim_vtservice 
 
 # Linker
 LINKER_FORCED_SHIM_LIBS := /system/vendor/bin/thermal|libshim_ifc.so
-LINKER_FORCED_SHIM_LIBS += /system/vendor/lib/libgui_ext.so|libshim_gui.so:/system/vendor/lib64/libgui_ext.so
-LINKER_FORCED_SHIM_LIBS += /system/vendor/lib/libMtkOmxVenc.so|libshim_ui.so:
+LINKER_FORCED_SHIM_LIBS += /system/vendor/lib/libgui_ext.so|libshim_gui.so:/system/vendor/lib64/libgui_ext.so|libshim_gui.so
+LINKER_FORCED_SHIM_LIBS += /system/vendor/lib/libMtkOmxVenc.so|libshim_ui.so:/system/vendor/lib/libui_ext.so|libshim_ui.so:/system/vendor/lib64/libui_ext.so|libshim_ui.so
+LINKER_FORCED_SHIM_LIBS += /system/vendor/lib/libcam_utils.so|libshim_ui.so
+LINKER_FORCED_SHIM_LIBS += /system/vendor/bin/guiext-server|libshim_xlog.so
+LINKER_FORCED_SHIM_LIBS += /system/lib/libandroid_runtime.so|libshim_misc.so:/system/lib64/libandroid_runtime.so|libshim_misc.so:/system/vendor/bin/program_binary_builder|libshim_misc.so
+LINKER_FORCED_SHIM_LIBS += /system/vendor/lib/libvtmal.so|libshim_vtservice.so
