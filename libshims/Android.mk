dc40b4ffb35a5471f51fea46acba381a4a601bb6
Pipaolo <mastastah8975@gmailcom>
2018-07-07 08:47:35 +0800

    Shim cam

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -39,3 +39,9 @@ LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_MODULE := libshim_xlog
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := camHack.cpp
+LOCAL_MODULE := libshim_cam
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
